83a1e7f97af724efee337e7b07bccb40ab6216c0
Drak <drak@zikula.org>
2010-06-20 08:12:54 +0000

    correction to error reporting
    git-svn-id: https://code.zikula.org/svn/phpcs/trunk@43 eb0acb78-538b-4529-bf16-1004ea9cc00c

--- Zikula/ZikulaCodingStandard.php
+++ Zikula/ZikulaCodingStandard.php
@@ -2,7 +2,7 @@
 if (class_exists('PHP_CodeSniffer_Standards_CodingStandard', true) === false) {
     throw new PHP_CodeSniffer_Exception('Class PHP_CodeSniffer_Standards_CodingStandard not found');
 }
-ini_set('error_reporting', E_ALL & ~ E_NOTICE);
+ini_set('error_reporting', E_ALL & ~E_NOTICE);
 
 /**
  * Zikula Coding Standard.
